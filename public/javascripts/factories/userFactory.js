38a2dd591ccc27e1864309c4d4597c08bc6201e5
Vincent <vincentckhuang@gmail.com>
2016-09-06 22:07:58 -0700

    added comments to userFactory

--- public/javascripts/factories/userFactory.js
+++ public/javascripts/factories/userFactory.js
@@ -11,33 +11,27 @@
 		// connect to socket
 		var socket = io();
 
-		factory.login
-
+		// variables updated with controllers
+		factory.login;
 		factory.name;
 		factory.password;
 
+		// game variables
 		factory.team;
 		factory.bullets = [];
 		factory.zombies = [];
+		var frames = 15;
+		var canvas;
+		var ctx;
 
 		var r = Math.floor(Math.random()*155)+100;
 		var g = Math.floor(Math.random()*155)+100;
 		var b = Math.floor(Math.random()*155)+100;
 		var randomColor = `rgb(${r},${g},${b})`;
-		console.log(randomColor);
 		// var randomColor = '#' + Math.floor(Math.random()*16777215).toString(16);
-
-		var bottom;
-		var right;
-
-		var frames = 15;
-
-		var canvas;
-		var ctx;
 
 		socket.on('get-socket', function(data) {
 			factory.socket = data.socket;
-			console.info(factory.socket);
 		})
 
 		socket.on('connect-room', function(data) {
@@ -46,15 +40,14 @@
 			$state.transitionTo('game.start.lobby');
 			setTimeout(function() {
 			factory.room.forEach(function(player) {
-				console.log(player);
 				var name = $(`#${player.name}-name`).css('color', player.color);
 				var image = $(`#${player.name}-image`).css('background-color', player.color);
 			})
 			},500)
 		})
 
+		// ---| GAME LOGIC |--- \\
 		socket.on('start-game', function(team) {
-
 			// movement
 			document.addEventListener('keydown', function(e) {
 				if (e.keyCode === 87 || e.keyCode === 68 || e.keyCode === 83 || e.keyCode === 65) {
@@ -69,40 +62,102 @@
 			})
 			$state.transitionTo('game.start.playing');
 
-
 			factory.team = team;
-			// console.log(factory.team);
-			// $rootScope.$broadcast('startGame');
+
 			setTimeout(function() {
+				// set canvas up
 				canvas = document.getElementById('game_canvas');
 				ctx = canvas.getContext('2d');
 				var border = $('#game');
 				ctx.canvas.width = border.width();
 				ctx.canvas.height = border.height()*0.9;
-				// getDimensions();
-				// console.log(bottom);
+				// initialize team
 				factory.team.forEach(function(player) {
 					player.score = 0;
 					player.x = 50;
 					player.y = 300;
-					console.log(player);
-					factory.player = player;
 					ctx.fillStyle = player.color;
 					ctx.fillRect(player.x, player.y, 50, 50);
-					// element.css('top', bottom/2);
 				})
 				// spawn zombies
 				setInterval(function() {
 					spawnZombie();
-				}, 1000/team.length)
+				}, 1000/team.length) // scale spawning with number of players
 
 				// check collsion
 				setInterval(function() {
 					checkCollision();
 				},frames);
 			},200)
-
 		})
+
+						factory.sendMovement = function(key) {
+							socket.emit('move-player', {player:factory.name, key: key});
+						}
+
+						factory.sendShot = function(key) {
+							socket.emit('player-shoot', {player:factory.name, key: key, color: randomColor});
+						}
+
+						function spawnZombie() {
+							// var y = Math.floor(Math.random() * canvas.height-100);
+							var y = ((new Date().getTime()) % canvas.height)-100;
+							var remainder = y%50;
+							y = 50+y-remainder;
+							var ranNum = Math.floor(Math.random()*10000);
+							// create new zombie
+							var zombie = {id: ranNum, y: y, x:canvas.width};
+							factory.zombies.push(zombie);
+							// zombie movement
+							var intID = setInterval(function(){
+								// find zombie index
+								var zomIndex;
+								factory.zombies.forEach(function(zombie, index) {
+									if(zombie.id === ranNum) {
+										zomIndex = index;
+									}
+								})
+								// save intID into zombie object
+								factory.zombies[zomIndex].intID = intID;
+								// move zombie left
+								factory.zombies[zomIndex].x -= 2;
+							}, frames)
+						}
+
+						function checkCollision() {
+							// bullet zombie collision
+							factory.bullets.forEach(function(bullet, bIndex) {
+								factory.zombies.forEach(function(zombie, zIndex) {
+									if (bullet.y > zombie.y && bullet.y < zombie.y+50 &&
+											bullet.x+5 > zombie.x) {
+										// remove from arrays
+										factory.bullets.splice(bIndex, 1);
+										factory.zombies.splice(zIndex, 1);
+										// clear intervals
+										clearInterval(bullet.intID);
+										clearInterval(zombie.intID);
+										// add score to player
+										factory.team.forEach(function(player, pIndex) {
+											if (bullet.owner === player.name) {
+												factory.team[pIndex].score += 5;
+											}
+										})
+									}
+								})				
+							})
+
+							// player zombie collision
+							factory.team.forEach(function(player, pIndex) {
+								factory.zombies.forEach(function(zombie) {
+									if (player.x+25 > zombie.x && player.x+25 < zombie.x+50 &&
+											player.y+25 > zombie.y && player.y+25 < zombie.y+50) {
+										factory.team.splice(pIndex,1);
+									}
+								})
+							})
+
+							drawAll();
+						}
 
 		socket.on('move-player', function(move) {
 			var playerIndex;
@@ -142,14 +197,16 @@
 			if (input.key === 'Space') {
 				var ranNum = Math.floor(Math.random()*10000);
 				// create projectile object
-				var bullet = {id: ranNum,
-											x: factory.team[playerIndex].x,
-											y: factory.team[playerIndex].y+24,
-											color: factory.team[playerIndex].color,
-											owner: factory.team[playerIndex].name};
+				var bullet = 
+					{id: ranNum,
+						x: factory.team[playerIndex].x,
+						y: factory.team[playerIndex].y+24,
+						color: factory.team[playerIndex].color,
+						owner: factory.team[playerIndex].name
+					};
 				// add bullet to array
 				factory.bullets.push(bullet); 				
-				// set movement
+				// set up movement
 				var intID = setInterval(function() {
 					// store bullet index
 					var bulletIndex;
@@ -162,24 +219,59 @@
 					factory.bullets[bulletIndex].intID = intID;
 					// move bullet right
 					factory.bullets[bulletIndex].x += distancePerTick;
+					// check if bullet has reached the edge of the canvas
 					if (factory.bullets[bulletIndex].x > canvas.width) {
-						// remove bullet from array when boundary is hit
+						// deduct 2 points from player for spamming 
+						factory.team.forEach(function(player, pIndex) {
+							if (factory.bullets[bulletIndex].owner === player.name) {
+								factory.team[pIndex].score -= 2;
+							}
+						})
+						// remove bullet from array
 						factory.bullets = factory.bullets.filter(function(bullet) {
 							if (bullet.id != ranNum) {
 								return bullet;
 							}
 						})
 						clearInterval(intID);
-						// deduct 2 points from player for spamming 
-						// factory.team.forEach(function() {
-							
-						// })
 					}
 				}, frames)
 			}
-			console.log(factory.bullets.length)
 		})
 
+		// ---| CANVAS FUNCTIONS |--- \\
+		function drawAll() {
+			// clear canvas
+			ctx.clearRect(0,0,canvas.width, canvas.height);
+			drawPlayers();
+			drawBullets();
+			drawZombies();
+		}
+
+		function drawPlayers() {
+			factory.team.forEach(function(player) {
+				ctx.fillStyle = player.color;
+				ctx.fillRect(player.x, player.y, 50, 50);
+				ctx.font = '24px serif';
+				ctx.strokeText(player.score, player.x+5, player.y+30)
+			})
+		}
+
+		function drawBullets() {
+			factory.bullets.forEach(function(bullet) {
+				ctx.fillStyle = bullet.color;
+				ctx.fillRect(bullet.x, bullet.y, 10, 3);
+			})
+		}
+
+		function drawZombies() {
+			factory.zombies.forEach(function(zombie) {
+				ctx.fillStyle = 'green';
+				ctx.fillRect(zombie.x, zombie.y, 50, 50);
+			})
+		}
+
+		// ---| GENERAL |--- \\
 		socket.on('error', function(data) {
 			console.warn(data.msg);
 			factory.error = data.msg;
@@ -229,6 +321,13 @@
 				}
 			})
 		}
+						function addPlayer(player,token) {
+							// keep user data in factory
+							factory.user = player;
+							socket.emit('add-player', {name:player.name, socket: player.socket, class: player.class, exp: player.experience, color: randomColor, token: token})
+							// switch to game state
+							$state.transitionTo('game.home')
+						}
 
 		factory.isLoggedIn = function() {
 			if (factory.name) {
@@ -260,143 +359,6 @@
 		factory.startGame = function() {
 			socket.emit('start-game', factory.room);
 		}
-
-		factory.sendMovement = function(key) {
-			socket.emit('move-player', {player:factory.name, key: key});
-		}
-
-		factory.sendShot = function(key) {
-			socket.emit('player-shoot', {player:factory.name, key: key, color: randomColor});
-		}
-
-		function addPlayer(player,token) {
-			// keep user data in factory
-			factory.user = player;
-			socket.emit('add-player', {name:player.name, socket: player.socket, class: player.class, exp: player.experience, color: randomColor, token: token})
-			// switch to game state
-			$state.transitionTo('game.home')
-		}
-
-		function spawnZombie() {
-			// console.log(new Date().getTime());
-			// var y = Math.floor(Math.random() * canvas.height-100);
-			var y = ((new Date().getTime()) % canvas.height)-100;
-			var remainder = y%50;
-			y = 50+y-remainder;
-			console.log(y);
-			var ranNum = Math.floor(Math.random()*10000);
-			// create new zombie
-			var zombie = {id: ranNum, y: y, x:canvas.width};
-			factory.zombies.push(zombie);
-			// zombie movement
-			var intID = setInterval(function(){
-				// find zombie index
-				var zomIndex;
-				factory.zombies.forEach(function(zombie, index) {
-					if(zombie.id === ranNum) {
-						zomIndex = index;
-					}
-				})
-				// save intID into zombie object
-				factory.zombies[zomIndex].intID = intID;
-				// move zombie left
-				factory.zombies[zomIndex].x -= 2;
-			}, frames)
-		}
-
-		function checkCollision() {
-			// bullet zombie collision
-			factory.bullets.forEach(function(bullet, bIndex) {
-				factory.zombies.forEach(function(zombie, zIndex) {
-					if (bullet.y > zombie.y && bullet.y < zombie.y+50 &&
-							bullet.x+5 > zombie.x) {
-						// remove from arrays
-						factory.bullets.splice(bIndex, 1);
-						factory.zombies.splice(zIndex, 1);
-						// clear intervals
-						clearInterval(bullet.intID);
-						clearInterval(zombie.intID);
-						// add score to player
-						factory.team.forEach(function(player, pIndex) {
-							if (bullet.owner === player.name) {
-								factory.team[pIndex].score += 5;
-							}
-						})
-					}
-				})				
-			})
-
-			// player zombie collision
-			factory.team.forEach(function(player, pIndex) {
-				factory.zombies.forEach(function(zombie) {
-					if (player.x+25 > zombie.x && player.x+25 < zombie.x+50 &&
-							player.y+25 > zombie.y && player.y+25 < zombie.y+50) {
-						factory.team.splice(pIndex,1);
-					}
-				})
-			})
-
-			drawAll();
-		}
-
-			// function checkCollisionMelee(weapon) {
-			// 	var weapon_length = 50
-			// 	var zombies = $('.zombie');
-			// 	var weaponPos = weapon.position();
-			// 	var weaponEnd = weaponPos.left + weapon_length;
-			// 	var weaponID = weapon.attr('id');
-			// 	var playerName = weaponID.substring(0,weaponID.indexOf('_'));
-			// 	for (var j=0; j<zombies.length; j++) {
-			// 		var zomPos = $(zombies[j]).position();
-			// 		var zomHeight = $(zombies[j]).height();
-			// 		var zomWidth = $(zombies[j]).width();
-			// 		// check to see if bullet position and zombie position range match
-			// 		if (weaponEnd > zomPos.left && weaponEnd < (zomPos.left+zomWidth) &&
-			// 				weaponPos.top > zomPos.top && weaponPos.top < (zomPos.top+zomHeight) ) {
-			// 			weapon.remove();
-			// 			$(zombies[j]).remove();
-			// 			factory.team.forEach(function(player) {
-			// 				if(player.name === playerName) {
-			// 					player.score += 5;
-			// 				}
-			// 			})
-			// 			console.log(factory.team[0].score);
-			// 		}
-			// 	}
-			// }
-
-			// CANVAS FUNCTIONS
-
-			function drawAll() {
-				// clear canvas
-				ctx.clearRect(0,0,canvas.width, canvas.height);
-				drawPlayers();
-				drawBullets();
-				drawZombies();
-			}
-
-			function drawPlayers() {
-				factory.team.forEach(function(player) {
-					ctx.fillStyle = player.color;
-					ctx.fillRect(player.x, player.y, 50, 50);
-					ctx.font = '24px serif';
-					ctx.strokeText(player.score, player.x+5, player.y+30)
-				})
-			}
-
-			function drawBullets() {
-				factory.bullets.forEach(function(bullet) {
-					ctx.fillStyle = bullet.color;
-					ctx.fillRect(bullet.x, bullet.y, 10, 3);
-				})
-			}
-
-			function drawZombies() {
-				factory.zombies.forEach(function(zombie) {
-					ctx.fillStyle = 'green';
-					ctx.fillRect(zombie.x, zombie.y, 50, 50);
-				})
-			}
 
 		return factory;
 	}
